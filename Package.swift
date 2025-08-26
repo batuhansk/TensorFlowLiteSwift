c21b15f6609ffcdcc33940a3012ff9ffaac392bd
Batuhan Saka <batuhannsskk@gmail.com>
2025-08-27 01:27:10 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
             targets: ["TensorFlowLite"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/kewlbear/TensorFlowLiteC.git", .branch("master")),
+        .package(url: "https://github.com/kewlbear/TensorFlowLiteC.git", .exact("2.13.0")),
     ],
     targets: [
         .target(
